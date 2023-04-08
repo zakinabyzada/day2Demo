7532de3ab21e079556dafc7b9a1a3290e5a2c06c
zaki <zaki@localhost>
2023-04-07 17:55:11 -0700

    James bond 007

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -2,6 +2,17 @@ package day2;
 
 public class Day2Demo {
 	public static void main(String[] args) {
-		
+
 	}
+
+	public static void day2Method() {
+		System.out.println("Zoom github push test 1 ");
+	}
+
+	public int classcount() {
+		int result = 0;
+
+		return result;
+	}
+
 }
